1b46942c0d8c375284f753c3074f6841b6be0141
Ark <arkarmoemyint100@gmail.com>
2024-07-21 22:19:53 -0400

    implement Sidebars

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,17 +1,18 @@
 import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar'
 import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
-  const loggedIn = {firstname: "Arkar"}
+  const loggedIn = {firstName: "Arkar", lastName: "M", email: "arkarmoemyint100@gmail.com"}
   return (
-    <section className = "home">
-      <div className = "home-content">
-        <header className = "home-header">
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
           <HeaderBox 
-            type = "greeting"
+            type="greeting"
             title="Welcome"
-            user={loggedIn?.firstname || "Guest"}
+            user={loggedIn?.firstName || "Guest"}
             subtext="Access and manage your account and transactions efficiently."
           />
           <TotalBalanceBox
@@ -20,7 +21,15 @@ const Home = () => {
             totalCurrentBalance={1250.35}
           />
         </header>
+
+        RECENT TRANSACTIONS
       </div>
+
+      <RightSidebar 
+        user={loggedIn}
+        transactions={[]}
+        banks={[{currentBalance: 123.50}, {currentBalance: 500.50}]}
+      />
     </section>
   )
 }
