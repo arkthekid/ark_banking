fe694c68600831f054732031abba4cb6d102ff7a
Ark <arkarmoemyint100@gmail.com>
2024-07-22 21:28:08 -0400

    Implement Appwrite AuthForm

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -21,12 +21,13 @@ import { Input } from "@/components/ui/input"
 import CustomInput from './CustomInput'
 import { Loader2 } from 'lucide-react'
 import { useRouter } from 'next/navigation'
-import { signIn, signUp } from '@/lib/actions/user.actions'
+import { getLoggedInUser, signIn, signUp } from '@/lib/actions/user.actions'
 
 const AuthForm = ({type}: {type:string}) => {
     const router = useRouter();
     const [user, setUser] = useState(null);
     const [isLoading, setIsLoading] = useState(false);
+    
     const formSchema = authFormSchema(type);
 
         // 1. Define your form.
@@ -52,12 +53,12 @@ const AuthForm = ({type}: {type:string}) => {
                     setUser(newUser);
                 }
                 if (type === 'sign-in'){
-                    // const response = await signIn({
-                    //     email: data.email,
-                    //     password: data.password,
-                    // })
+                    const response = await signIn({
+                        email: data.email,
+                        password: data.password,
+                    })
 
-                    // if(response) router.push('/')
+                    if(response) router.push('/')
                 }
             }
             catch (error) {
