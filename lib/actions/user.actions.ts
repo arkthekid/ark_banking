b03c1a590372fb397f2b00dafad6929700321d1d
Ark <arkarmoemyint100@gmail.com>
2024-07-22 21:11:16 -0400

    Implement Appwrite Auth

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,5 +1,10 @@
 'use server'
 
+import { ID } from "node-appwrite";
+import { createAdminClient, createSessionClient } from "../appwrite";
+import { cookies } from "next/headers";
+import { parseStringify } from "../utils";
+
 export const signIn = async () => {
     try {
         //Mutation / Database / Make fetch
@@ -9,9 +14,40 @@ export const signIn = async () => {
 }
 
 export const signUp = async (userData: SignUpParams) => {
+    const { email, password, firstName, lastName } = userData;
+
     try {
-        //Mutation / Database / Make fetch
+        const { account } = await createAdminClient();
+
+        // Create the email password session
+        const newUserAccount = await account.create(
+            ID.unique(), 
+            email, 
+            password, 
+            `${firstName} ${lastName}`
+        );
+
+        const session = await account.createEmailPasswordSession(email, password);
+
+        // Set the session cookie
+        cookies().set("my-custom-session", session.secret, {
+            path: "/",
+            //expires: new Date(session.expire),
+            sameSite: "strict",
+            secure: true,
+            httpOnly: true,
+        });
+        return parseStringify(newUserAccount);
     } catch (error) {
         console.error("Error", error);
+    }
+}
+
+export async function getLoggedInUser() {
+    try {
+        const { account } = await createSessionClient();
+        return await account.get();
+    } catch (error) {
+        return null;
     }
 }
